e06b6302406435d518ba01a68fbb79eb55b7253d
Ben <ben.sho@hotmail.com>
2019-11-05 17:46:31 +1100

    using callback

--- src/comp3431-rsa/comp3431_starter/src/nodes/drive.py
+++ src/comp3431-rsa/comp3431_starter/src/nodes/drive.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 from skimage import morphology
-from simple_pid import PID
+#from simple_pid import PID
 import perpective_transform
 from matplotlib import pyplot as plt
 from glob import glob
@@ -24,21 +24,23 @@ import numpy as np
 from locate import contours_from_range, find_beacon
 
 from geometry_msgs.msg import Twist
-
-def getCameraData():
-    # TODO check if subscribed topics correct
-    """
-    Subscriber function for camera data
-    :return: RGB and depth data
-    """
-    pixel_data = None
-    while pixel_data is None:
-        try:
-            pixel_data = rospy.wait_for_message("/camera/rgb/image_rect_color", Image, timeout=1)
-        except Exception as e:
-            print(e)
-            pass
-
+from time import time
+count = 0
+start = 0
+def callback(pixel_data):
+    
+    CENTER = 397
+    TOP = 699
+    WIDTH = 38
+    global count
+    global start
+    count += 1
+    print(count)
+    if count == 1:
+        start = time()
+    if count == 20:
+        print("time = ", time() - start)
+        exit()
     pts1 = np.float32([[0,480],[640,480],[0,295],[640,295]])
     pts2 = np.float32([[360,700],[450,700],[0,0],[640,380]])
 
@@ -48,6 +50,81 @@ def getCameraData():
     M = cv2.getPerspectiveTransform(pts1,pts2)
 
     dst = cv2.warpPerspective(img,M,(640,700))
+    img = dst
+
+    m = mask(img, (150, 150, 150), (255, 255, 255))
+    m = cv2.cvtColor(m, cv2.COLOR_RGB2GRAY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    m = cv2.dilate(m, kernel, iterations=10)
+    m[m>0] = 255
+    #img = morphology.thin(m//255)
+    img = m
+
+    # new ###########################
+    lefts, rights = [], []
+    for height in range(50):
+        for width in range(60):
+            if img[TOP-height, CENTER-width]:
+                lefts.append(width)
+                break
+        for width in range(60):
+            if img[TOP-height, CENTER+width]:
+                rights.append(width)
+                break
+    rights = []
+    if lefts and rights:
+        mid = (2*CENTER - int(np.mean(lefts)) + int(np.mean(rights)))//2
+    elif lefts:
+        mid = CENTER - int(np.mean(lefts)) + WIDTH
+    elif rights:
+        mid = CENTER + int(np.mean(rights)) - WIDTH
+    else:
+        return
+    control = mid - CENTER
+    # control = pid(control)/100.0
+    control = control/100.0
+    print("at:", mid)
+    print("control:", control)
+    img[670:690, mid] = True
+    if abs(control) < 0.3:
+        twist.linear.x = 0.20
+    else:
+        twist.linear.x = 0.10
+
+    twist.linear.y = twist.linear.z = 0
+    twist.angular.x = twist.angular.y = 0
+    twist.angular.z = -control#-float(err) / 100
+
+    cmd_vel_pub.publish(twist)
+
+    print("turn by:", control)
+    bridge = CvBridge()
+    img = img.astype("uint8")
+    # # img[img == True] = 255
+    img *= 255
+    # #print(img[40])
+    # cv_image = img
+    #print(img)
+    image_pub = rospy.Publisher("image_topic_2", Image)
+    msg = bridge.cv2_to_imgmsg(img, "mono8")
+    #print(msg)
+    image_pub.publish(msg)
+
+def getCameraData(pixel_data):
+    # TODO check if subscribed topics correct
+    """
+    Subscriber function for camera data
+    :return: RGB and depth data
+    """
+    # pixel_data = None
+    # while pixel_data is None:
+    #     try:
+    #         pixel_data = 
+    #     except Exception as e:
+    #         print(e)
+    #         pass
+    print("got camera data")
+    
     return dst
 
 
@@ -90,69 +167,68 @@ if __name__ == "__main__":
                         Twist, queue_size=1)
 
     twist = Twist()
-
+    rospy.Subscriber("camera/rgb/image_rect_color", Image, callback)
     rospy.init_node("drive")
-    CENTER = 397
-    TOP = 699
-    WIDTH = 47
-    pid = PID(1, 0.1, 0.05, setpoint=0, proportional_on_measurement =True)
-    pid.output_limits = (-100, 100)
-    while True:
-        img = getCameraData()
+    
+    # pid = PID(1, 0.1, 0.05, setpoint=0, proportional_on_measurement =True)
+    # pid.output_limits = (-100, 100)
+    rospy.spin()
+    # while True:
+    #     img = getCameraData()
 
 
-        m = mask(img, (150, 150, 150), (255, 255, 255))
-        m = cv2.cvtColor(m, cv2.COLOR_RGB2GRAY)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-        m = cv2.dilate(m, kernel, iterations=10)
-        m[m>0] = 255
-        img = morphology.skeletonize(m//255)
+        # m = mask(img, (150, 150, 150), (255, 255, 255))
+        # m = cv2.cvtColor(m, cv2.COLOR_RGB2GRAY)
+        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        # m = cv2.dilate(m, kernel, iterations=10)
+        # m[m>0] = 255
+        # img = morphology.skeletonize(m//255)
 
-        # new ###########################
-        lefts, rights = [], []
-        for height in range(50):
-            for width in range(60):
-                if img[TOP-height, CENTER-width]:
-                    lefts.append(width)
-                    break
-            for width in range(60):
-                if img[TOP-height, CENTER+width]:
-                    rights.append(width)
-                    break
-        rights = []
-        if lefts and rights:
-            mid = (2*CENTER - int(np.mean(lefts)) + int(np.mean(rights)))//2
-        elif lefts:
-            mid = CENTER - int(np.mean(lefts)) + WIDTH
-        elif rights:
-            mid = CENTER + int(np.mean(rights)) - WIDTH
-        else:
-            mid = False
-        control = mid - CENTER
-        # control = pid(control)/100.0
-        control = control/50.0
-        print("at:", mid)
-        print("control:", control)
-        img[670:690, mid] = True
-        twist.linear.x = 0.05
-        twist.linear.y = twist.linear.z = 0
-        twist.angular.x = twist.angular.y = 0
-        twist.angular.z = control#-float(err) / 100
+        # # new ###########################
+        # lefts, rights = [], []
+        # for height in range(50):
+        #     for width in range(60):
+        #         if img[TOP-height, CENTER-width]:
+        #             lefts.append(width)
+        #             break
+        #     for width in range(60):
+        #         if img[TOP-height, CENTER+width]:
+        #             rights.append(width)
+        #             break
+        # rights = []
+        # if lefts and rights:
+        #     mid = (2*CENTER - int(np.mean(lefts)) + int(np.mean(rights)))//2
+        # elif lefts:
+        #     mid = CENTER - int(np.mean(lefts)) + WIDTH
+        # elif rights:
+        #     mid = CENTER + int(np.mean(rights)) - WIDTH
+        # else:
+        #     continue
+        # control = mid - CENTER
+        # # control = pid(control)/100.0
+        # control = control/200.0
+        # print("at:", mid)
+        # print("control:", control)
+        # img[670:690, mid] = True
+        # twist.linear.x = 0.05
+        # twist.linear.y = twist.linear.z = 0
+        # twist.angular.x = twist.angular.y = 0
+        # twist.angular.z = -control#-float(err) / 100
 
         # cmd_vel_pub.publish(twist)
 
-        print("turn by:", control)
-        bridge = CvBridge()
-        img = img.astype("uint8")
-        # # img[img == True] = 255
-        img *= 255
-        # #print(img[40])
-        # cv_image = img
-        #print(img)
-        image_pub = rospy.Publisher("image_topic_2", Image)
-        msg = bridge.cv2_to_imgmsg(img, "mono8")
-        #print(msg)
-        image_pub.publish(msg)
+        # print("turn by:", control)
+        # bridge = CvBridge()
+        # img = img.astype("uint8")
+        # # # img[img == True] = 255
+        # img *= 255
+        # # #print(img[40])
+        # # cv_image = img
+        # #print(img)
+        # image_pub = rospy.Publisher("image_topic_2", Image)
+        # msg = bridge.cv2_to_imgmsg(img, "mono8")
+        # #print(msg)
+        # image_pub.publish(msg)
 
         # # new ###########################
 
