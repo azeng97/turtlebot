dd9b15032cc44663a5e01a5b1b36ab202791d204
Avan Zeng <azeng97@gmail.com>
2019-11-24 23:48:12 +1100

    working lane map

--- src/comp3431-rsa/comp3431_starter/src/nodes/path_node.py
+++ src/comp3431-rsa/comp3431_starter/src/nodes/path_node.py
@@ -26,31 +26,35 @@ class NavPath(object):
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
         self.visited = [[0 for n in range(100)] for k in range(100)]
         self.origin = [49, 49]
+        self.origin_odom = None
 
     def callback(self, msg):
         #rospy.loginfo(msg)
-        transform = self.tfBuffer.lookup_transform("map", "base_link", rospy.Time(0), rospy.Duration(1.0))
         transform2 = self.tfBuffer.lookup_transform("base_link", "odom", rospy.Time(0), rospy.Duration(1.0))
+        # if not self.origin_odom:
+        #     self.origin_odom = [msg.pose.pose.position.x, msg.pose.pose.position.y]
+        # msg.pose.pose.position.x -= self.origin_odom[0]
+        # msg.pose.pose.position.y -= self.origin_odom[1]
         pose_base = tf2_geometry_msgs.do_transform_pose(msg.pose, transform2)
-        pose_base.pose.position.y -= 0.2
-        pose_transformed = tf2_geometry_msgs.do_transform_pose(pose_base, transform)
+        left, pose_transformed, right = self.get_points(pose_base)
         if not self._path:
             self._path.append(pose_transformed.pose.position)
-            marker = self.create_marker(self._path, len(self._paths))
-            self._paths.append(marker)
+            self._left.append(left.pose.position)
+            self._right.append(right.pose.position)
             self.add_history(pose_transformed.pose.position)
-        elif not self.has_visited(pose_transformed.pose.position):
+            self.create_road()
+        if not self.has_visited(pose_transformed.pose.position):
             last = self._path[-1]
             coords = (pose_transformed.pose.position.x, pose_transformed.pose.position.y)
             if np.linalg.norm((last.x-coords[0], last.y-coords[1])) > 0.3:
                 self._path = []
-                marker = self.create_marker(self._path, len(self._paths))
-                self._paths.append(marker)
+                self._left = []
+                self._right = []
             self._path.append(pose_transformed.pose.position)
-            self._paths[-1] = self.create_marker(self._path, len(self._paths))
-            #print(len(self._path))
+            self._left.append(left.pose.position)
+            self._right.append(right.pose.position)
             self.add_history(pose_transformed.pose.position)
-
+            self.create_road()
         self.show_path_in_rviz(marker_publisher, self._paths)
 
     def add_history(self, pos):
@@ -64,7 +68,7 @@ class NavPath(object):
     def has_visited(self, pos):
         return self.visited[int(pos.x*10+self.origin[0])][int(pos.y*10+self.origin[1])]
 
-    def create_marker(self, points, id):
+    def create_marker(self, points, id, r, scale):
         marker = Marker(
                     type=Marker.LINE_STRIP,
                     id=id,
@@ -72,16 +76,34 @@ class NavPath(object):
                     header=Header(frame_id='map')
                     )
         marker.pose.orientation.w = 1.0
-        marker.scale.x = 0.02
+        marker.scale.x = scale
         marker.color.b = 0.0
-        marker.color.g = 0.0
-        marker.color.r = 1.0
+        marker.color.g = 1-r
+        marker.color.r = r
         marker.color.a = 1.0
         marker.action = marker.ADD
         return marker
 
-    def add_markers(self, left, path, right):
-        self._paths.append()
+    def create_road(self):
+        idx = len(self._paths)
+        if len(self._path) == 1:
+            self._paths.append(self.create_marker(self._left, idx, 1, 0.05))
+            self._paths.append(self.create_marker(self._path, idx + 1, 0, 0.02))
+            self._paths.append(self.create_marker(self._right, idx + 2, 1, 0.05))
+        else:
+            self._paths[-3] = self.create_marker(self._left, idx, 1, 0.05)
+            self._paths[-2] = self.create_marker(self._path, idx + 1, 0, 0.02)
+            self._paths[-1] = self.create_marker(self._right, idx + 2, 1, 0.05)
+
+    def get_points(self, pose_base):
+        transform = self.tfBuffer.lookup_transform("map", "base_link", rospy.Time(0), rospy.Duration(1.0))
+        pose_base.pose.position.y -= 0.2
+        pose_path = tf2_geometry_msgs.do_transform_pose(pose_base, transform)
+        pose_base.pose.position.y -= 0.4
+        pose_right = tf2_geometry_msgs.do_transform_pose(pose_base, transform)
+        pose_base.pose.position.y += 0.8
+        pose_left = tf2_geometry_msgs.do_transform_pose(pose_base, transform)
+        return pose_left, pose_path, pose_right
 
     def show_path_in_rviz(self, marker_publisher, paths):
         marker_publisher.publish(paths)
